8a6e554ca8cd490d7c76b9919ae0dcf56717840a
khodaeidev <khodaei.dev@gmail.com>
2024-08-09 12:37:17 +0330

    Develop logics of project

--- key-code/js/script.js
+++ key-code/js/script.js
@@ -1,11 +1,25 @@
-﻿let $ = document
-let locationKey = $.querySelector('#location')
-let title = $.querySelector('title')
+﻿// let title = document.querySelector("title");
 
-document.body.addEventListener('keydown', function (event) {
-	
-	starter.style.display = 'none'
-	heading.style.display = 'flex'
-	ascii.style.display = 'flex'
-	infos.style.display = 'flex'
-})
+//Codes
+let keyCode = document.getElementById("keyCode");
+let key = document.getElementById("key");
+let keyLocation = document.getElementById("location");
+let which = document.getElementById("which");
+let code = document.getElementById("code");
+
+document.body.addEventListener("keydown", function (event) {
+  starter.style.display = "none";
+  heading.style.display = "flex";
+  ascii.style.display = "flex";
+  infos.style.display = "flex";
+});
+
+document.body.addEventListener("keydown", function (event) {
+  event.preventDefault();
+  code.innerHTML = event.code;
+  key.innerHTML = event.key;
+  keyLocation.innerHTML = event.location;
+  which.innerHTML = event.which;
+  keyCode.innerHTML = event.keyCode;
+  console.log(event);
+});
