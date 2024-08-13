beb06efd4a49e43c68e85a1d5d8760ac125fbc3e
khodaeidev <khodaei.dev@gmail.com>
2024-08-13 14:51:03 +0330

    Dev Notification box for Add Not Err

--- note-app/js/app.js
+++ note-app/js/app.js
@@ -4,10 +4,23 @@ let saveBtn = $.getElementById("btn-save");
 let deleteBtn = $.getElementById("btn-delete");
 let colors = $.querySelectorAll(".color-box");
 let noteList = $.getElementById("listed");
+let notification = document.getElementById("notification");
+let notificationTxt = document.getElementById("notificationTxt");
 
+//* Notification
+function showNotification(err) {
+  notificationTxt.innerHTML = err;
+  notification.classList.add("show");
+  setTimeout(hideNotification, 3000); // Hide the notification after 3 seconds
+}
+function hideNotification() {
+  notification.classList.remove("show");
+}
+
+//* Add Note
 function addNote() {
   if (inputElem.value === "") {
-    return alert("Please write a note ");
+    return showNotification("Please write a note");
   }
   let note = inputElem.value;
 
